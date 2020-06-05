e19a904ec659a3776f9353c941252ae71abd8b99
NikolaMedaric <63855981+NikolaMedaric@users.noreply.github.com>
2020-06-05 17:36:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiceRoller dice = new DiceRoller(20);
+            for( int i=0; i<20;i++)
+            {
+                dice.InsertDie(new Die(6));
+            }
+            dice.RollAllDice();
+            IList<int> results = dice.GetRollingResults();
+            foreach (int result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
